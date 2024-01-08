8e24790a2f41ae00ee89149a3ad8a70b6a81fe2c
maurice <maurice.maina@students.jkuat.ac.ke>
2024-01-08 12:05:02 +0300

    updates

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -4,8 +4,48 @@ const resultDisplay=document.getElementById("result");
 
 const possibleChoices=document.querySelectorAll("button");
 let userChoice;
+let computerChoice;
+let result;
 
 possibleChoices.forEach(possibleChoice=>possibleChoice.addEventListener("click",e=>{
     userChoice=e.target.id;
     userChoiceDisplay.innerHTML=userChoice;
+    generateComputerChoice();
+    getResult();
 }));
+
+function generateComputerChoice(){
+    const randomNumber=Math.floor(Math.random() * possibleChoices.length)+1;
+    
+    if (randomNumber===1){
+        computerChoice='rock'
+    }
+    else if (randomNumber===2){
+        computerChoice='paper'
+    }
+    else{
+        computerChoice='scissors'
+    }
+
+    computerChoiceDisplay.innerHTML=computerChoice;
+}
+
+function getResult(){
+    if(computerChoice===userChoice){
+        result="It's a draw!";
+    }
+
+    else if(computerChoice==='rock' && userChoice==='scissors' || computerChoice==='paper' && userChoice==='rock' || computerChoice==='scissors' && userChoice==='paper'){
+        result='You lost!';
+    }
+    // else if(computerChoice==='paper' && userChoice==='rock'){
+    //     result='You lost';
+    // }
+    // else if(computerChoice==='scissors' && userChoice==='paper'){
+    //     result='You lost';
+    // }
+    else{
+        result='You won!';
+    }
+    resultDisplay.innerHTML=result;
+}
